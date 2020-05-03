96ba833c03e6322c175647dbef90f9521f1805de
MishaWork <42887014+MishaWork@users.noreply.github.com>
2020-05-03 11:15:35 +0300

    Add files via upload

--- Snowbo.cpp
+++ Snowbo.cpp
@@ -2,31 +2,31 @@
  #include "TXLib.h"
 
  void AlphaBlend (HDC Dest, int xDest, int yDest, int wDest, int hDest, HDC Src,  int xSrc,  int ySrc,  int wSrc,  int hSrc, double alpha);
- double Distance (double TreeX, double TreeY, double SnoX, double SnoY, int SisyaX);
+ double Distance (double TreeX, double TreeY, double SnoX, double SnoY, int SNOX);
 
  int main()
      {
      txCreateWindow (800, 600);
 
-     HDC Jopa = txLoadImage ("unnamed.bmp");
-     if (Jopa == NULL) txMessageBox ("Ты плоская баба");
+     HDC Backg = txLoadImage ("unnamed.bmp");
+     if (Backg == NULL) txMessageBox ("Ты плоская баба");
 
-     HDC Sisya = txLoadImage ("Snowboard1.bmp");
-     if (Sisya == NULL) txMessageBox ("Ты кошачий лоток");
+     HDC SNO = txLoadImage ("Snowboard1.bmp");
+     if (SNO == NULL) txMessageBox ("Ты кошачий лоток");
 
-     HDC Kaka = txLoadImage ("Tree.bmp");
-     if (Kaka == NULL) txMessageBox ("У вас запор");
+     HDC Tree = txLoadImage ("Tree.bmp");
+     if (Tree == NULL) txMessageBox ("У вас запор");
 
      int SourceY1 = 0;
 
-     int JopaY = txGetExtentY (Jopa) / 34;
+     int BackgY = txGetExtentY (Backg) / 34;
 
-     int JopaX = txGetExtentX (Jopa);
+     int BackgX = txGetExtentX (Backg);
 
-     int SisyaX = txGetExtentX (Sisya) / 19;
+     int SNOX = txGetExtentX (SNO) / 19;
 
-     int TreeSX = txGetExtentX (Kaka) / 1;
-     int TreeSY = txGetExtentY (Kaka) / 1;
+     int TreeSX = txGetExtentX (Tree) / 1;
+     int TreeSY = txGetExtentY (Tree) / 1;
 
      //int Move = 0;
 
@@ -45,55 +45,90 @@
      double SnoX = 0;
      double SnoY = 360;
 
-     double Mult = 0.5;
+     double ScaleTree = 0.5;
+
+     double MoveV = 1;
+
+     double T = 0;
+
+     double BackgV = 1;
+
+     double TreeV = 0.05;
+
+     double TreeVX = 10;
+
+     double TreeVY = 1.9;
+
+     int StopT = 0;
 
      txBegin ();
 
      //AlphaBlend (txDC(), 0, 0, 600, 450, Jopa, 0, 0, 368, , 1);
      while (1 < 2)
         {
-        SourceY1 += JopaY;
+        //SourceY1 = SourceY * JopaY;
 
-        txBitBlt (txDC(), 0, 0, 0, JopaY, Jopa, 0, SourceY1);
+        T += BackgV;
+
+        txBitBlt (txDC(), 0, 0, 0, BackgY, Backg, 0, int (T) % 34 * BackgY);
 
         //AlphaBlend (txDC(), 0, 0, 800, 600, Jopa, 0, SourceY1, JopaX, JopaY, 1);
 
-        txAlphaBlend (txDC(), SnoX+200, SnoY, SisyaX, 0, Sisya, (Ang + 9) * SisyaX);
+        txAlphaBlend (txDC(), SnoX+200, SnoY, SNOX, 0, SNO, (Ang + 9) * SNOX);
 
-        AlphaBlend (txDC(), TreeX - 92*Mult, TreeY - 280*Mult, Mult * TreeSX, Mult * TreeSY, Kaka, 0, 0, TreeSX, TreeSY, 1);
+        AlphaBlend (txDC(), TreeX - 92*ScaleTree, TreeY - 280*ScaleTree, ScaleTree * TreeSX, ScaleTree * TreeSY, Tree, 0, 0, TreeSX, TreeSY, 1);
 
         txSetColor (TX_LIGHTGREEN);
 
-        txCircle (SnoX+200+SisyaX/2, SnoY+70, 10);
+        txCircle (SnoX+200+SNOX/2, SnoY+70, 10);
 
         txSetColor (TX_PINK);
 
         txCircle (TreeX, TreeY, 10);
 
-        TreeY += 10;
-        TreeX -= 1.9;
+        TreeY += TreeVX;
+        TreeX -= TreeVY;
 
-        Mult += 0.05;
+        ScaleTree += TreeV;
 
-        double DistanceST = Distance (TreeX, TreeY, SnoX, SnoY, SisyaX);
+        double DistanceST = Distance (TreeX, TreeY, SnoX, SnoY, SNOX);
 
-        if (DistanceST < 70) TreeY = 300, TreeX = 400, Mult = 0.5, SnoX = SnoX+200+SisyaX/2, SnoY + 70;
+        if (DistanceST < 70)
+            {
+            TreeY = 300,
+            TreeX = 400,
+            ScaleTree = 0.5,
+            SnoX = SnoX+200+SNOX/2,
+            SnoY + 70,
+            BackgV = 0,
+            TreeV = 0,
+            TreeVX = 0,
+            TreeVY = 0,
+            StopT = GetTickCount ();
+            }
+
+        if (GetTickCount () - StopT > 7000)
+            {
+            BackgV = 1,
+            TreeV = 0.05,
+            TreeVX = 10,
+            TreeVY = 1.9;
+            }
 
         printf ("Я ем соленый металолом %lg\n", DistanceST);
 
-        if (TreeY > 700) TreeY = 300, TreeX = 400, Mult = 0.5;
-
-        txLine (100, 0, 100, 600);
-        txLine (700, 0, 700, 600);
-
-        if (SourceY1 > 8500) SourceY1 = 0;
-
-        //if (GetAsyncKeyState ('A')) Move = Move - 10;
-        //if (GetAsyncKeyState ('D')) Move = Move + 10;
-
-        if  (GetAsyncKeyState (VK_LEFT))  angV =  1;
-        if  (GetAsyncKeyState (VK_RIGHT)) angV = -1;
-
+        if (TreeY > 700) TreeY = 300, TreeX = 400, ScaleTree = 0.5;
+                                                                         //0 0
+        txLine (100, 0, 100, 600);                                       //1 1
+        txLine (700, 0, 700, 600);                                       //2 0
+                                                                         //3 1
+        //if (T > 34) T = 0;                                               //4 0
+                                                                         //5 1
+                                                                         //6 0
+                                                                         //7 1
+        if  (GetAsyncKeyState (VK_LEFT))  angV =  1;                     //8 0
+        if  (GetAsyncKeyState (VK_RIGHT)) angV = -1;                     //9 1
+                                                                         //10 0
         if (SnoX > 200) SnoX = 0;
         if (SnoX < -300) SnoX = 0;
 
@@ -122,9 +157,9 @@ void AlphaBlend (HDC Dest, int xDest, int yDest, int wDest, int hDest,
                        Src,  xSrc,  ySrc,   wSrc,  hSrc, blend);
     }
 
-double Distance (double TreeX, double TreeY, double SnoX, double SnoY, int SisyaX)
+double Distance (double TreeX, double TreeY, double SnoX, double SnoY, int SNOX)
     {
-    double DistanceST = sqrt ((SnoX+200+SisyaX/2 - TreeX) * (SnoX+200+SisyaX/2 - TreeX) + (SnoY + 70 - TreeY) * (SnoY + 70 - TreeY));
+    double DistanceST = sqrt ((SnoX+200+SNOX/2 - TreeX) * (SnoX+200+SNOX/2 - TreeX) + (SnoY + 70 - TreeY) * (SnoY + 70 - TreeY));
 
     return DistanceST;
     }
